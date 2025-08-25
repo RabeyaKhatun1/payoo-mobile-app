3ac12d032d03378270c0b370dc830cf7fe455427
Rabeya Khatun <prorabeya2008@gmail.com>
2025-08-25 23:43:15 +0600

    main

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ document.getElementById('login-btn')
    .addEventListener('click', function(e){
     e.preventDefault()
     const mobileNumber = 12345678910;
-    const pinNumber = 1234;
     const mobileNumberValue = document.getElementById('mobile-number').value
     const mobileNumberValueConverted = parseInt(mobileNumberValue)
 
+    const pinNumber = 1234;
     const pinNumberValue = document.getElementById('pin-number').value
     const pinNumberValueConverted = parseInt(pinNumberValue)
 
